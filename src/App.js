9bc57cfdcac2bcfcf60bbaa3cbf15251d0aa5948
Samirkumar Tripathy <samir.tripathy@euclidinnovations.com>
2019-09-05 17:27:37 +0530

    Signup page in progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
-import AdminComponent from './Admin/AdminComponent.js';
-import AnswerComponent from './Answer/AnswerComponent.js';
-import QuestionComponent from './Question/QuestionComponent.js';
-import CommonComponent from './Common/CommonComponent.js';
+// import AdminComponent from './Admin/AdminComponent.js';
+// import AnswerComponent from './Answer/AnswerComponent.js';
+// import QuestionComponent from './Question/QuestionComponent.js';
+// import CommonComponent from './Common/CommonComponent.js';
 import UserComponent from './User/UserComponent.js';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <AdminComponent />
+        {/* <AdminComponent />
         <AnswerComponent />
         <QuestionComponent />
-        <CommonComponent />
+        <CommonComponent /> */}
         <UserComponent />
       </div>
     );
